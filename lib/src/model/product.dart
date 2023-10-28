2ad86c39ccb59da80dddf408edd395ed6a7dc218
soomy1997 <57267548+soomy1997@users.noreply.github.com>
2023-10-28 18:44:41 +0300

    first commit

---
+++ lib/src/model/product.dart
@@ -0,0 +1,18 @@
+class Product {
+  int id;
+  String? name;
+  String? category;
+  String? image;
+  double? price;
+  bool? isliked;
+  bool isSelected;
+  Product({
+    required this.id,
+    this.name,
+    this.category,
+    this.price,
+    this.isliked,
+    this.isSelected = false,
+    this.image,
+  });
+}
